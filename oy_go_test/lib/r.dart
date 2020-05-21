da4273ef9fd37f569fe84bed652bba6ec4b4e4f5
gaojun <804716454@qq.com>
2020-05-21 12:26:25 +0800

    插件管理

---
+++ oy_go_test/lib/r.dart
@@ -0,0 +1,12 @@
+class R {
+  /// ![](http://127.0.0.1:9527/res/images/animated_flutter_lgtm.gif)
+  static final String resImagesAnimatedFlutterLgtm = 'res/images/animated_flutter_lgtm.gif';
+  /// ![](http://127.0.0.1:9527/res/images/dart_side.png)
+  static final String resImagesDartSide = 'res/images/dart_side.png';
+  /// ![](http://127.0.0.1:9527/res/images/launcher_icon.png)
+  static final String resImagesLauncherIcon = 'res/images/launcher_icon.png';
+  /// ![](http://127.0.0.1:9527/res/images/material_design_3.png)
+  static final String resImagesMaterialDesign3 = 'res/images/material_design_3.png';
+  /// ![](http://127.0.0.1:9527/res/images/material_design_4.jpg)
+  static final String resImagesMaterialDesign4 = 'res/images/material_design_4.jpg';
+}
