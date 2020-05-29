2cd7455ea438fb5d2b47019f354dbc3e4138a6ee
gaojun <804716454@qq.com>
2020-05-29 21:00:08 +0800

    分支文件夹

--- oy_go_test/lib/my_main_app.dart
+++ oy_go_test/lib/my_main_app.dart
@@ -12,6 +12,7 @@ import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:oy_go_test/themes.dart';
 import 'package:oy_go_test/constant.dart';
+import 'r.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -36,10 +37,9 @@ class _MySplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      constraints: BoxConstraints.expand(),
-      color: Colors.white,
-      child: Center(child: AppIcon),
-    );
+        constraints: BoxConstraints.expand(),
+        color: Colors.white,
+        child: Image.asset(R.resImagesAnimatedFlutterLgtm));
   }
 }
 
@@ -48,7 +48,9 @@ class _MyMainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter Catalog",
-      theme: Provider.of<MyAppSettings>(context).isDarkMode ? darkTheme : lightThemme,
+      theme: Provider.of<MyAppSettings>(context).isDarkMode
+          ? darkTheme
+          : lightThemme,
       routes: AppRoutingTabel,
     );
   }
